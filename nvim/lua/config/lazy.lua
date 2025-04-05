3acf4a5bb7b646c2cb214276dd5267c16a104b73
Anthony Gonzales <3740655+antgonzales@users.noreply.github.com>
2025-04-04 23:57:59 -0400

    Fix the order of plugin imports
    Update dependencies.

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,10 +18,11 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
-    { import = "plugins" },
+    -- import LazyVim extras
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    -- import/override with your plugins
+    { import = "plugins" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
