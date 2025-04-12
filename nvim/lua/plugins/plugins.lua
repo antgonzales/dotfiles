f3ec63940d35e6e2200888b6fa603febd0128460
Anthony Gonzales <3740655+antgonzales@users.noreply.github.com>
2025-04-12 01:37:35 -0400

    Set HNVM_NODE in environment variables

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -8,21 +8,6 @@ return {
       colorscheme = "rose-pine-moon",
     },
   },
-  -- Fix issue with hnvm and the lsp server when moving between contexts
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      setup = {
-        vtsls = function(_, opts)
-          -- Inject the HNVM_NODE variable so vtsls picks the right Node version.
-          opts.cmd_env = {
-            HVNM_QUIET = false, -- Ensure we log hnvm info in cases of failure.
-            HNVM_NODE = "20.16.0", -- Update this version to match your hnvm setup.
-          }
-        end,
-      },
-    },
-  },
   {
     -- change some telescope options and a keymap to browse plugin files
     {
