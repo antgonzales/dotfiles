5ae87dc65c377eb545aa13b2da0ea14b840ad5b6
Anthony Gonzales <3740655+antgonzales@users.noreply.github.com>
2025-11-05 10:49:47 -0500

    Update terminal and convert to dark mode
    Update Ghostty terminal settings. Update LazyVim to utilize
    the same settings.

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -1,35 +1,51 @@
 return {
-  -- Load Rose Pine colorscheme
-  { "rose-pine/neovim" },
-  -- Set colorscheme (will be managed by auto-dark-mode)
+  -- Rose Pine colorscheme with transparency support
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000,
+    opts = {
+      disable_background = true,
+      disable_float_background = true,
+    },
+    config = function(_, opts)
+      require("rose-pine").setup(opts)
+      
+      -- Automatically detect theme from terminal background
+      if vim.o.background == "dark" then
+        vim.cmd("colorscheme rose-pine-moon")
+      else
+        vim.cmd("colorscheme rose-pine-dawn")
+      end
+    end,
+  },
+  -- LazyVim configuration
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme managed by auto-dark-mode plugin
+      -- Let Rose Pine handle colorscheme selection
     },
   },
+  -- Telescope configuration
   {
-    -- change some telescope options and a keymap to browse plugin files
-    {
-      "nvim-telescope/telescope.nvim",
-      keys = {
-        -- Update <leader>ff to search only in the current working directory
-        {
-          "<leader>ff",
-          function()
-            require("telescope.builtin").find_files({ cwd = vim.fn.getcwd() })
-          end,
-          desc = "Find Files in Current Directory",
-        },
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- Update <leader>ff to search only in the current working directory
+      {
+        "<leader>ff",
+        function()
+          require("telescope.builtin").find_files({ cwd = vim.fn.getcwd() })
+        end,
+        desc = "Find Files in Current Directory",
       },
-      -- change some options
-      opts = {
-        defaults = {
-          layout_strategy = "horizontal",
-          layout_config = { prompt_position = "top" },
-          sorting_strategy = "ascending",
-          winblend = 0,
-        },
+    },
+    -- change some options
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
       },
     },
   },
